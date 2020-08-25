f1c9e5f5ec5814dd9dfb6ee7094b813b7f891246
Andy Barrett <apbarret@nsidc.org>
2020-08-24 18:31:50 -0600

    add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='sunlight_usi',
+    url='',
+    author='Andy Barrett',
+    author_email='andypbarrett@gmail.com',
+    # Needed to actually package something
+    packages=['sunlight_usi'],
+    # Needed for dependencies
+    install_requires=['numpy','matplotlib','cartopy','pandas','xarray'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='MIT',
+    description='Tools for Sunlight Under Sea Ice project',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
