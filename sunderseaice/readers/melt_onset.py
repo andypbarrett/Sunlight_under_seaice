a9958f2f56cf2e01660e0795b01637fefba4c83a
Andy Barrett <apbarret@nsidc.org>
2020-08-27 17:29:06 -0600

    fix read_ps to work on earlier versions of xarray

--- sunderseaice/readers/melt_onset.py
+++ sunderseaice/readers/melt_onset.py
@@ -1,6 +1,7 @@
 # Reads Polar Stereographic melt onset grids
 import glob
 import os
+from packaging import version
 
 import numpy as np
 import xarray as xr
@@ -31,12 +32,19 @@ def read_ps(dirpath):
     
     year = get_years_from_filenames(filelist)
 
-    da = xr.open_mfdataset(filelist, combine='nested', concat_dim='year')
-    da.coords['year'] = year
-    da.coords['x'] = get_ps_coords('x')
-    da.coords['y'] = get_ps_coords('y')
+    # Workaround until rasterio installed on sunlight_under_seaice
+    if version.parse(xr.__version__) <= version.parse('0.12.1'):
+        args = dict(paths=filelist, concat_dim='year')
+    else:
+        args = dict(paths=filelist, combine='nested', concat_dim='year')
+        
+    ds = xr.open_mfdataset(**args)
+    ds.coords['year'] = year
+    ds.coords['x'] = get_ps_coords('x')
+    ds.coords['y'] = get_ps_coords('y')
+    ds.attrs.pop('Year', None)  # year is spurious attribute
     
-    print(da)
-    
+    return ds
+
 
     
