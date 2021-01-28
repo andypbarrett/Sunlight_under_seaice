db5b19982dd01f644aaf4338df67048a0b8f3e84
Andy Barrett <apbarret@nsidc.org>
2021-01-28 16:11:36 -0700

    add verbose to shell command

--- sunderseaice/snowmodel_gdat_to_netcdf.sh
+++ sunderseaice/snowmodel_gdat_to_netcdf.sh
@@ -11,4 +11,4 @@ CTLFILE="$DATAPATH/$REANALYSIS/SM_snod_merra2_01Aug1980-31Jul2018.ctl"
 # Output netcdf filepath
 NCFILE="$DATAPATH/$REANALYSIS/SnowModel_snow_depth_merra2_01Aug1980-31Jul2018.nc"
 
-python gdat_to_netcdf.py $GDATFILE $CTLFILE $NCFILE $VARIABLE $REANALYSIS
+python gdat_to_netcdf.py $GDATFILE $CTLFILE $NCFILE $VARIABLE $REANALYSIS --verbose
