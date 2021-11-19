b06dcd8a7712dc003389de0c5d2c11b5a6b80f2d
Andy Barrett <apbarret@nsidc.org>
2021-11-19 15:08:10 -0700

    add atl10 reader

--- sunderseaice/readers/icesat2.py
+++ sunderseaice/readers/icesat2.py
@@ -1,5 +1,6 @@
 """Module to read ICESat-2 files into xarray.Dataset"""
 
+import h5py
 import xarray as xr
 
 
@@ -17,4 +18,21 @@ def load_atl20_month(filepath):
         ])
     ds = ds.set_coords(['grid_lat', 'grid_lon'])
     return ds
+
+
+def atl10(filepath):
+    '''Read ATL10 (Freeboard)'''
+    f = h5py.File(filepath, 'r')
+    gt2l = xr.Dataset({
+        'freeboard': (['x'], f['gt2l']['freeboard_beam_segment']['beam_freeboard']['beam_fb_height'][:]), 
+        'geoseg_beg': (['x'], f['gt2l']['freeboard_beam_segment']['beam_freeboard']['geoseg_beg'][:]), 
+        'geoseg_end': (['x'], f['gt2l']['freeboard_beam_segment']['beam_freeboard']['geoseg_end'][:]), 
+        'latitude': (['x'], f['gt2l']['freeboard_beam_segment']['beam_freeboard']['latitude'][:]), 
+        'longitude': (['x'], f['gt2l']['freeboard_beam_segment']['beam_freeboard']['longitude'][:])
+    },)
+    # Add segment center as alongtrack distance coordinate
+    x = (gt2l.geoseg_beg + gt2l.geoseg_end.values) * 0.5
+    gt2l.coords['x'] = x.values
+    return gt2l
+
 
