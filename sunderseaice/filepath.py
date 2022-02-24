231f666c6e1f62d5ba7dbd61da45f23973a2b972
Andy Barrett <apbarret@nsidc.org>
2022-02-24 12:32:46 -0700

    fix granule list path

--- sunderseaice/filepath.py
+++ sunderseaice/filepath.py
@@ -5,4 +5,4 @@ from pathlib import Path
 HOME = Path.home()
 ICESAT2_PATH = HOME / 'Data' / 'ICESat-2'
 
-GRANULE_LIST_PATH = HOME / 'src' / 'SunlightUnderSeaIce' / 'sunderseaice'
+GRANULE_LIST_PATH = HOME / 'src' / 'SunlightUnderSeaIce' / 'sunderseaice' / 'data'
